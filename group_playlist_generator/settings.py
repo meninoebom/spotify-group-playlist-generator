2bb5c0c23a13103705cef7178ed5fa879bd84bae
Brandon Brown <brandon.brown@ymail.com>
2015-06-02 19:04:58 -0700

    updates settings and bower config stuff

--- group_playlist_generator/settings.py
+++ group_playlist_generator/settings.py
@@ -77,12 +77,36 @@ WSGI_APPLICATION = 'group_playlist_generator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'group_playlist_generator',
+# Databases for Elastic Beanstalk Deployment
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'group_playlist_generator',
+        }
+    }
+
+# Databases for EC2 Deployment
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '<DB Name>',
+#         'HOST': 'localhost',
+#         'USER': '<DB User>',
+#         'PASSWORD': '<DB Password>'
+#     }
+# }
 
 
 # Internationalization
@@ -103,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
